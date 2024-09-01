3c4103f498d0e79f3774af438d41d30d97ccd97d
issa isabar <issa.isabar@hotmail.com>
2024-09-01 16:35:24 +0200

    minor changes

---
+++ Locus.Data/Models/InsuranceInformation.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace Locus.Data.Models;
+public class InsuranceInformation : BindableObject
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+
+    private bool _isDescriptionVisible;
+    public bool IsDescriptionVisible
+    {
+        get => _isDescriptionVisible;
+        set
+        {
+            _isDescriptionVisible = value;
+            OnPropertyChanged();
+        }
+    }
+}
+
